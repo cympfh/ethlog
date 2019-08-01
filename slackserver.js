256b0fe8c28071aaf379a5eb1d4f2b7c73f35993
cympfh <cympfh@gmail.com>
2019-08-01 13:04:27 +0900

    improvement

--- slackserver.js
+++ slackserver.js
@@ -3,6 +3,25 @@ var web3 = new Web3("wss://ropsten.infura.io/ws");
 
 var config = require('./.secret.json');
 
+// last session
+const fs = require('fs');
+const session_file_path = './.secret.slacksession.json'
+
+function restore_session() {
+	if (fs.existsSync(session_file_path)) {
+		return require(session_file_path);
+	} else {
+		return {'block': 5474580, 'tx': 0};
+	}
+}
+
+var session = restore_session();
+
+function save_session() {
+	fs.writeFileSync(session_file_path, JSON.stringify(session));
+  console.log('Session saved');
+}
+
 //simplestorageのABI
 var abi = require('./build/contracts/EthLog.json');
 
@@ -10,21 +29,10 @@ var abi = require('./build/contracts/EthLog.json');
 var address = abi.networks['3'].address;
 const ethLog = new web3.eth.Contract(abi.abi, address, {});
 
-ethLog.getPastEvents("allEvents", {fromBlock: 0}).then((events) => {
+ethLog.getPastEvents("allEvents", {fromBlock: session.block}).then((events) => {
 	console.log("past events");
 	events.forEach(processEvent);
 });
-
-// Slack API
-function send(data) {
-  var request = require('request');
-  data.channel = config.slack.channel;
-  request({
-    uri: config.slack.webhook,
-    method: 'POST',
-    json: data
-  });
-}
 
 var event = ethLog.events.Speech();
 event.on('data', (ev) => {
@@ -37,30 +45,43 @@ event.on('error', (ev) => {
 	console.error(ev);
 });
 
-var buf = [];
+// Slack API
+function send(data) {
+	var request = require('request');
+	data.channel = config.slack.channel;
+	request({
+		uri: config.slack.webhook,
+		method: 'POST',
+		json: data
+	});
+}
 
 function send_update(username, text) {
-
-  var key = `${username}___${text}`;
-  if (buf.indexOf(key) > -1) { // duplicated
-    return;
-  }
-  buf.push(key);
-  while (buf.length > 100) buf.shift();
-
-  var payload = {
-    "icon_url": 'https://cdn4.iconfinder.com/data/icons/cryptocoins/227/ETH-512.png',
-    "username": `@${username}@ropsten`,
-    "text": `${text}`
-  };
-  send(payload);
+	var payload = {
+		"icon_url": 'https://cdn4.iconfinder.com/data/icons/cryptocoins/227/ETH-512.png',
+		"username": `@${username}@ropsten`,
+		"text": `${text}`
+	};
+	send(payload);
 }
 
 async function processEvent(event) {
+
+  console.log(session, event.blockNumber, event.transactionIndex);
+	if (session.block > event.blockNumber ||
+		(session.block == event.blockNumber && session.tx >= event.transactionIndex)) {
+    console.log('skip');
+		return;
+	}
+	session.block = event.blockNumber
+	session.tx = event.transactionIndex;
+
 	const sender = event.returnValues[0];
 	const body = event.returnValues[1];
 	const nickname = await ethLog.methods.getNickname(sender).call();
 	const username = web3.utils.hexToUtf8(nickname);
-	console.log(`${username}: ${body}`);
+	console.log(`${username}: ${body}`, event.blockNumber, event.transactionIndex);
 	send_update(username, body);
 }
+
+setInterval(save_session, 10000);
